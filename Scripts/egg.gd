2c3e049a85c6802f925c59455dec43c91a2b8adb
KapperKevin <absolutezeroka@gmail.com>
2026-09-21 20:00:15 -0700

    Resource updates

--- Scripts/egg.gd
+++ Scripts/egg.gd
@@ -1,7 +1,16 @@
 class_name Egg
 extends Resource
 
+enum EggType {STAT_EFFECT, PHYSICAL_EFFECT}
+enum EggRarity {COMMON, RARE, MYTHIC, QUEST, CURSED}
+
+
 @export var egg_name: String = "None"
 @export var egg_sprite: Texture2D
 @export var gold_value: int = 0
 @export var rep_value: int = 0
+@export var stat_effect: float = 0.0
+@export var hit_point: int = 1
+@export var effect_type: EggType = EggType.STAT_EFFECT
+@export var rarity: EggRarity = EggRarity.COMMON
+@export var egg_scene: PackedScene
