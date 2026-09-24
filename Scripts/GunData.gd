a2deb54fe51d5429862e3e12757aca3997542454
KapperKevin <absolutezeroka@gmail.com>
2026-09-23 18:09:31 -0700

    GunData script

---
+++ Scripts/GunData.gd
@@ -0,0 +1,14 @@
+class_name GunData
+extends Resource
+
+@export_category("Bullet Stats")
+@export var gun_damage: int = 1
+@export var gun_fire_rate: float = 0.5
+@export var gun_overheat: int = 0
+@export var bullet_velocity: int = 1
+@export var bullet_pierce: int = 0
+@export var bullet_bounces: int = 0
+
+@export_category("Visual")
+@export var bullet_projectile: PackedScene
+@export var firing_animation: Animation
