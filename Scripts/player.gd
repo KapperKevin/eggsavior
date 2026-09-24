218b90117d5187b78d4e4a1deeabc75d0140fe9c
KapperKevin <absolutezeroka@gmail.com>
2026-09-23 18:48:21 -0700

    Pistol updates, Beta weapon Equip

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
 @export var stats: PlayerStats
+@export var weapon_scene: PackedScene
+
+var weapon_spawn_node: Node2D
 var speed: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	speed = stats.current_player_speed
-
-
+	#Equiping Gun
+	if weapon_scene:
+		weapon_spawn_node = weapon_scene.instantiate()
+		add_child(weapon_spawn_node)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	movementLoop()
@@ -29,4 +34,7 @@ func movementLoop() -> void:
 func collect_egg(egg: Egg) -> void:
 	stats.apply_effect(egg)
 	speed = stats.current_player_speed
+	
+func shoot() -> void:
+	pass
 	
