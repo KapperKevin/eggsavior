a1314657023eaab973a13347515049ea19e99da2
KapperKevin <absolutezeroka@gmail.com>
2026-09-24 19:00:10 -0700

    Bullet Physics Beta

--- Scripts/pistol_projectile.gd
+++ Scripts/pistol_projectile.gd
@@ -1,7 +1,6 @@
-extends Area2D
-
-var velocity: Vector2 = Vector2.ZERO
+extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	# Move the bullet forward every frame based on its velocity
 	position += velocity * delta
+	move_and_slide()
